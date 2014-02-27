b167461d953e5315f60f89873ac1c68f348588a1
Hugo Peixoto <hugo.peixoto@gmail.com>
2014-02-27 02:07:27 +0000

    Adds setup.py and friends

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='plowshare',
+    version='0.1.0',
+    author='Hugo Peixoto',
+    author_email='hugo.peixoto@gmail.com',
+    packages=['plowshare'],
+    scripts=[],
+    url='https://github.com/super3/plowshare-wrapper',
+    license='LICENSE.txt',
+    description='Manages multi-host uploads using plowshare.',
+    long_description=open('README.md').read(),
+    install_requires=[],
+    )

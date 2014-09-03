0c8d601fb74cc99de78bb665e5aff988fc3a549d
Ivan Miric <imiric@gmail.com>
2014-09-03 22:33:32 +0200

    Fix import errors in tool.py
    The explicit relative import change in 88bf7b3 caused issues with
    running tool.py in plowshare/ (ValueError: Attempted relative import in
    non-package). Moving it to the /bin directory with the import changes
    here fixes it.

--- bin/tool.py
+++ bin/tool.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
-
-import plowshare
 import json
 import sys
+
+from plowshare import Plowshare
 
 if __name__ == "__main__":
     filename    = sys.argv[1]
     host_number = int(sys.argv[2])
 
-    result = plowshare.Plowshare().upload(filename, host_number)
+    result = Plowshare().upload(filename, host_number)
 
     print json.dumps(result)

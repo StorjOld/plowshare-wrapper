cdaed5c3f545987e5bf1b91c264e83e8a5d2e2d0
Josh Brandoff <josh@joshua.is>
2014-12-01 17:14:27 -0500

    Remove unused local settings import. This is Django style anyways.

--- plowshare/settings.py
+++ plowshare/settings.py
@@ -1,7 +1,3 @@
-# Minimum upload success percentage of available hosts to ensure proper file redundancy
-MIN_FILE_REDUNDANCY      = 0.6
-
-try:
-    from local_settings import *
-except ImportError:
-    pass
+# Minimum upload success percentage of available hosts to ensure proper
+# file redundancy
+MIN_FILE_REDUNDANCY = 0.6

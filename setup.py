b5c8a6936521674d3f3dba93ecd062e5183829e3
Josh Brandoff <josh@joshua.is>
2014-12-01 16:50:28 -0500

    Clean up setup and make it work with pytest.

--- setup.py
+++ setup.py
@@ -1,20 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Storj Labs
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+version = '0.3.0'
+
+test_requirements = [
+    'pytest',
+    'pytest-pep8',
+    'pytest-cache',
+    'coveralls'
+]
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # Import PyTest here because outside, the eggs are not loaded.
+        import pytest
+        import sys
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setup(
     name='plowshare',
-    version='0.3.0',
+    version=version,
+    url='https://github.com/storj/plowshare-wrapper',
+    download_url='https://github.com/storj/plowshare-wrapper/tarball/'+version
+    license='MIT',
     author='Hugo Peixoto',
     author_email='hugo.peixoto@gmail.com',
-    packages=['plowshare'],
-    scripts=[],
-    url='https://github.com/super3/plowshare-wrapper',
-    license='LICENSE.txt',
     description='Manages multi-host uploads using plowshare.',
-    long_description=open('README.md').read(),
-    install_requires=[],
-    extras_require={
-        'develop': [
-            'tox',
-        ]
-    }
+    packages=['plowshare'],
+    cmdclass={'test': PyTest},
+    tests_require=test_requirements,
+    keywords = ['storj', 'metadisk', 'plowshare', 'plowshare wrapper']
 )

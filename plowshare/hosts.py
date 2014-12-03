9eb3b02ace01d128349246a264e8f64008dd41a7
Josh Brandoff <josh@joshua.is>
2014-12-03 07:16:25 -0500

    Use single quotes.

--- plowshare/hosts.py
+++ plowshare/hosts.py
@@ -24,10 +24,10 @@
 # SOFTWARE.
 
 anonymous = [
-    "euroshare_eu",
-    "ge_tt",
-    "gfile_ru",
-    "multiupload",
-    "rghost",
-    "zalil_ru"
+    'euroshare_eu',
+    'ge_tt',
+    'gfile_ru',
+    'multiupload',
+    'rghost',
+    'zalil_ru'
 ]
